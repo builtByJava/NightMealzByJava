c6b7fcb73a8051c48ca0f25aebe40cc05cdd8633
BlackRain29 <55949931+BlackRain29@users.noreply.github.com>
2023-06-20 20:29:12 -0700

    modified the styling and functionality on Orders page, added in a universal food search feature on the homepage, improved and modified the existing styling

--- src/API.java
+++ src/API.java
@@ -3,4 +3,6 @@ public class API {
     //This API will handle all incoming requests from the client UI that will be written in AngularJS
     //This API will also assist with doing some data processing with a database
 
+
+
 }
